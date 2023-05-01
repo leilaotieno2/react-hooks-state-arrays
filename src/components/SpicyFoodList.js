986f0d22bf92f043342ee916ebe56e90ff825e87
leilaotieno2 <leila.otieno@student.moringaschool.com>
2023-05-01 17:04:28 +0300

    first commit

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -2,11 +2,12 @@ import React, { useState } from "react";
 import { spicyFoods, getNewRandomSpicyFood } from "../data";
 
 function SpicyFoodList() {
-  const [foods, setFoods] = useState(spicyFoods);
+  const [foods] = useState(spicyFoods);
 
   function handleAddFood() {
     const newFood = getNewRandomSpicyFood();
     console.log(newFood);
+
   }
 
   const foodList = foods.map((food) => (
